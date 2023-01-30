fadc982e683e779f2aa5e0856b3df802590c7066
Jimjinny919 <xowls919@naver.com>
2023-01-30 17:40:25 +0900

    AWS Day04~05
    Google,Naver 로그인 추가

--- src/main/java/org/mega/book/springboot/web/IndexController.java
+++ src/main/java/org/mega/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.mega.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.mega.book.springboot.config.auth.LoginUser;
+import org.mega.book.springboot.config.auth.dto.SessionUser;
 import org.mega.book.springboot.service.PostsService;
 import org.mega.book.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model , @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        return "index";
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            System.out.println(user.getName());
+        model.addAttribute("userName", user.getName());
     }
+        return"index";
+}
+
+
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
